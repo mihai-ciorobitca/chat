ba6ac81bf08094e5267dfb00bbaddeb8ac249995
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-06-09 14:47:55 +0300

    n:

---
+++ chat.py
@@ -0,0 +1,22 @@
+from flask_socketio import SocketIO, emit
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def test_connect():
+    print('Client connected')
+
+@socketio.on('message')
+def handle_message(message):
+    print('message received:'+ message)
+    emit('message', {'message': message}, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)

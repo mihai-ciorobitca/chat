45545ee922af13806b5512e8c95c02602a67d22a
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2025-01-21 13:52:28 +0200

    upload

--- upload.bat
+++ upload.bat
@@ -1,6 +1,8 @@
-node init
-node install express
-node install path
+@echo on
+npm init -y && (
+call npm install express
+call npm install path
 git add .
 git commit -m "upload"
 git push origin master
+)

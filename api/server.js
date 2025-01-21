60bcc514c3cc1895d9601e957ed0c2a8edcbeab8
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2025-01-21 14:00:13 +0200

    upload

--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 
+require('dotenv').config();
  
 const app = express();
 const port = 3000;
+
+app.use(session({
+    secret: process.env.SECRET_KEY || 'secret',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+const mainRoutes = require('./routes/main');
+
+app.use('/', mainRoutes);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));

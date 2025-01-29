83109ee8e5a002d7adb38016533cf40be8153158
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2025-01-29 12:11:06 +0200

    upload

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -5,6 +5,7 @@ const session = require("express-session");
 
 router.use(express.json());
 
+const adminAuthClient = supabase.auth.admin;
 
 router.use(session({
     secret: 'your-secret-key',
@@ -15,7 +16,6 @@ router.use(session({
 
 router.get('/', async (req, res) => {
     if (req.session.admin) {
-        const adminAuthClient = supabase.auth.admin;
         const { data: { users }, error } = await adminAuthClient.listUsers()
         if (error) {
             console.log(error);
@@ -30,9 +30,20 @@ router.post("/", async (req, res) => {
     const { password } = req.body;
     if (password == "admin") {
         req.session.admin = true;
-        return res.status(200).json({ message: "Login successful" });;
+        return res.status(200).json({ message: "Login successful" });
     }
     return res.status(401).json({ message: "Invalid email or password" });
+});
+
+router.delete("/delete-user", async(req, res) => {
+    const { id } = req.body;
+    console.log(id);
+    const { data, error } = await supabase.auth.admin.deleteUser(id);
+    if (error) {
+        return res.status(500).json({message: error.message});
+    }      
+    console.log(data);
+    return res.status(200).json({ message: "Login successful" });;
 });
 
 module.exports = router;
